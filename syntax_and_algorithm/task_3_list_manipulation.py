0ddb3c1ccea0e3ae91e23cba88d2773734651969
Jethro Guzman <jethro.guzman@sminvestments.com>
2023-08-07 16:41:34 +0800

    move description to the functions, and restrict unique_elements

--- syntax_and_algorithm/task_3_list_manipulation.py
+++ syntax_and_algorithm/task_3_list_manipulation.py
@@ -1,38 +1,40 @@
 """
 Write the following function implementations:
-
-1. unique_elements -> takes a list of values and returns a new list with unique elements of the first list.
-
-Example:
-    [1, 2, 3, 3, 3, 3, 4, 5] -> [1, 2, 3, 4, 5]
-    ["a", "b", "c", "c"] -> ["a", "b", "c"]
-
-
-2. reverse_list -> takes a list of values and returns a new list with the elements in reversed order.
-- Do not use the built-in reversal functions.
-
-Example:
-    [1, 2, 3, 4, 5] -> [5, 4, 3, 2, 1]
-    ["a", "b", "c"] -> ["c", "b", "a"]
-
-
-3. max_element -> takes a list of values and returns the maximum value in the list.
-- Do not use the built-in max function.
-
-Example:
-    [1, 2, 3, 4, 5] -> 5
-    ["a", "b", "c"] -> "c"
 """
 
 
 def unique_elements(values):
+    """
+    1. unique_elements -> takes a list of values and returns a new list with unique elements of the first list.
+    - Do not use the built-in set function.
+
+    Example:
+        [1, 2, 3, 3, 3, 3, 4, 5] -> [1, 2, 3, 4, 5]
+        ["a", "b", "c", "c"] -> ["a", "b", "c"]
+    """
     pass
 
 
 def reverse_list(values):
+    """
+    2. reverse_list -> takes a list of values and returns a new list with the elements in reversed order.
+    - Do not use the built-in reversal functions.
+
+    Example:
+        [1, 2, 3, 4, 5] -> [5, 4, 3, 2, 1]
+        ["a", "b", "c"] -> ["c", "b", "a"]
+    """
     pass
 
 
 def max_element(values):
+    """
+    3. max_element -> takes a list of values and returns the maximum value in the list.
+    - Do not use the built-in max function.
+
+    Example:
+        [1, 2, 3, 4, 5] -> 5
+        ["a", "b", "c"] -> "c"
+    """
     pass
 
